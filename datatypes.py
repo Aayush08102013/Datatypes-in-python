333172a22d874517c1e0c65261d1d22f9940ff0e
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-02-28 18:52:29 +1300

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+# data types
+a = 12
+print("the type the value stored in a is:", type(a))
+w= 19.34
+print("the type of value that is stored in w is:", type(w))
+c= "coding" 
+print("the type of value that is stored in c is:", type(c))
+is_present = True
+print("the type of value that is stored in is_present is:", type(is_present))
+# type conversion 
+w1= int(w)
+print("after type conversion of a variable, the value is:", w1)
+# indexing
+inp=input("enter your string")
+rev_inp = inp[::-1]
+print(rev_inp)
